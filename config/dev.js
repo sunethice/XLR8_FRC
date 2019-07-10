e110a8023089e6eab2689b81f7c314a08054d5d1
Suneth Perera <sunethjoes@gmail.com>
2019-07-10 20:31:12 +1000

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID :'801848304258-livo5qaes65klhi1tv7d9lsbg55t1eet.apps.googleusercontent.com',
+    googleClientSecret: 'ZRCUg6BJTpbazlrlKpd650F7',
+    mongoURI: 'mongodb+srv://mDBUser:vR9FbQORsEBYCVwv@emaily-0uhh1.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'redacted'
+}
